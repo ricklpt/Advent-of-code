1dd9077352a4e1c70c961acf6a4802e9e39f7c1b
Rick Poot <rick.poot@incentro.com>
2020-12-01 08:01:09 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Advent_of_code
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			//new Day1();
+			new Day2();
+		}
+	}
+}
